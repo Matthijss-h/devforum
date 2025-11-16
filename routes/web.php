77d24d14603d227163826f48c4f452b59ba7e228
Matthijs Hulshof <hulshofmatthijs@gmail.com>
2025-11-16 16:25:18 +0100

    Removed unused routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,6 @@ Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('thread
 Route::middleware('auth')->group(function () {
     Route::get('/threads/{thread}/topics/create', [TopicController::class, 'create'])->name('topics.create');
     Route::post('/threads/{thread}/topics', [TopicController::class, 'store'])->name('topics.store');
-    Route::get('/threads/{thread}/topics/{topic}/edit', [TopicController::class, 'edit'])->name('topics.edit');
-    Route::patch('/threads/{thread}/topics/{topic}', [TopicController::class, 'update'])->name('topics.update');
-    Route::delete('/threads/{thread}/topics/{topic}', [TopicController::class, 'destroy'])->name('topics.destroy');
-    
-    // Reply routes
-    Route::post('/threads/{thread}/topics/{topic}/replies', [ReplyController::class, 'store'])->name('replies.store');
-    Route::delete('/threads/{thread}/topics/{topic}/replies/{reply}', [ReplyController::class, 'destroy'])->name('replies.destroy');
 });
 
 Route::get('/threads/{thread}/topics/{topic}', [TopicController::class, 'show'])->name('topics.show');
